2e018cb69fc566dff7b791ba09b4c871c5684afe
SangMin8703 <ls8703@gmail.com>
2023-02-13 23:37:39 +0900

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+import Darwin
+
+var greeting = "Hello, playground"
+
+var var1 : String = "variable"
+    
+print(var1);
+
+print("내가 지금 하는 말은 \(greeting)");
+
+print(greeting);
+
+
+let constant : String = "차후에 변경이 ㅜㄹ가능한 상수 let"
+
+var num1 : Int = 1;
+var num2 : Int = 2;
+
+func sum(num1 : Int, num2 : Int)->Int{
+    return num1 + num2;
+}
+
---
+++ PracticeProject/PracticeProject/PracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectApp.swift
+//  PracticeProject
+//
+//  Created by 이상민 on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
